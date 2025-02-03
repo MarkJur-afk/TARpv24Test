48553fa55a4d0f4c0d15b3421abaa10076e6994e
Opilane TTHK <opilane@tthk.ee>
2025-02-03 15:40:08 +0200

    ifelsecolor

---
+++ ifelsecolor/ifelsecolor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ifelsecolor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chose color: red, blue, green, white ");
+            string color = Console.ReadLine();
+            if (color == "red" || color == "blue" || color == "green" || color == "white")
+            {
+                Console.WriteLine("your color is " + color);
+            }
+            else
+            {
+                Console.WriteLine("you dont have this color!");
+            }
+
+        }
+    }
+}
