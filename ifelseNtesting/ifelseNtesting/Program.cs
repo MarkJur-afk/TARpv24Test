8d6eb8b26919d8c8ec205217d959e534f12969d3
Opilane TTHK <opilane@tthk.ee>
2025-02-03 14:48:40 +0200

    ifelsenumber2

--- ifelseNtesting/ifelseNtesting/Program.cs
+++ ifelseNtesting/ifelseNtesting/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Number control!");
+
+            int number = 192;
+            //kontroll kas arv on suurem 189-st
+
+            if (number > 189)
+            {
+                //kui vastab if tingimustele, siis n intab konsoolis seda arvu
+                Console.WriteLine("Number is bigger");
+
+                //if ja else nestimine
+                if (number >= 190 && number <= 200)
+                {
+                    Console.WriteLine("Number is between 190 to 200");
+                }
+                else
+                {
+                    Console.WriteLine("Number is bigger than 200");
+;               }
+            }
+            else
+            {
+                Console.WriteLine("Number is smaller");
+            }
         }
     }
 }
