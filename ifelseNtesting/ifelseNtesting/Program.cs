af3bfa97bf750b9caa205bbc345fdae2c8b0719f
Opilane TTHK <opilane@tthk.ee>
2025-02-03 13:59:17 +0200

    intin

---
+++ ifelseNtesting/ifelseNtesting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ifelseNtesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
