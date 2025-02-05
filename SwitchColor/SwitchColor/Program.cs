ac292e8e29b41bfe5955ff7c9c0839dbbfa47c34
Opilane TTHK <jurgenmarkk@gmail.com>
2025-02-05 15:47:35 +0200

    Switch Color

---
+++ SwitchColor/SwitchColor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SwitchColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
